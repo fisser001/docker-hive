264927f5ab64dc6cfa6aa34e1abb7516950ba46f
Fisser <mfisser@sva.de>
2019-02-21 10:26:39 +0100

    initial commit

--- imagebuildfile.sh
+++ imagebuildfile.sh
@@ -4,5 +4,5 @@ export version=1.0.0
 export hive_version=2.3.2
 
 cd ~/repos/masterarbeit/docker-hive
-docker build -t mfisser/hive:${version}-hive${hadoop_version}-for-postgresql-metastore ./
-docker push mfisser/hive:${version}-hive${hadoop_version}-for-postgresql-metastore
+docker build -t mfisser/hive:${version}-hive${hive_version}-for-postgresql-metastore ./
+docker push mfisser/hive:${version}-hive${hive_version}-for-postgresql-metastore
