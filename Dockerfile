e4b47d3b7d9e0a1d09b150dfc323969d0b79f529
Fisser <mfisser@sva.de>
2019-02-21 16:17:23 +0100

    enable tez

--- Dockerfile
+++ Dockerfile
@@ -37,6 +37,9 @@ ADD conf/hive-log4j2.properties $HIVE_HOME/conf
 ADD conf/ivysettings.xml $HIVE_HOME/conf
 ADD conf/llap-daemon-log4j2.properties $HIVE_HOME/conf
 
+RUN mkdir -p /opt/tmp_libs
+ADD libs/* /opt/tmp_libs
+
 COPY startup.sh /usr/local/bin/
 RUN chmod +x /usr/local/bin/startup.sh
 
--- startup.sh
+++ startup.sh
@@ -5,5 +5,13 @@ hadoop fs -mkdir -p    /user/hive/warehouse
 hadoop fs -chmod g+w   /tmp
 hadoop fs -chmod g+w   /user/hive/warehouse
 
+
+hdfs dfs -mkdir -p /hadoop/apps/tez/
+
+hdfs dfs -put /opt/tmp_libs/* /hadoop/apps/tez/
+hdfs dfs -chown -R $HDFS_USER:$HADOOP_USER /hadoop
+hadoop fs -chmod g+w /hadoop/apps/tez
+hadoop fs -chmod g+w /hadoop/apps/tez/*
+
 cd $HIVE_HOME/bin
 ./hiveserver2 --hiveconf hive.server2.enable.doAs=false
