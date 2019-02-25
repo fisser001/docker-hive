31f35266a8f73ce9847a7fd2fc1d04b49b086af9
Fisser <mfisser@sva.de>
2019-02-25 11:05:25 +0100

    add maven and build tez

--- startup.sh
+++ startup.sh
@@ -9,10 +9,11 @@ hadoop fs -chmod g+w   /user/hive/warehouse
 hdfs dfs -mkdir -p /apps/tez/
 hdfs dfs -chmod -R 777 /apps/tez/
 
-hdfs dfs -put /tmp/apache-tez-$TEZ_VERSION-bin.tar.gz /apps/tez/
+hdfs dfs -put /apache-tez-$TEZ_VERSION-src/tez-dist/target/tez-$TEZ_VERSION.tar.gz /apps/tez
+#hdfs dfs -put /tmp/apache-tez-$TEZ_VERSION-bin.tar.gz /apps/tez/
 hdfs dfs -chown -R $HDFS_USER:$HADOOP_USER /apps
 hadoop fs -chmod g+w /apps/tez
 hadoop fs -chmod g+w /apps/tez/*
 hdfs dfs -chmod -R 777 /apps/tez/
 cd $HIVE_HOME/bin
-./hiveserver2 --hiveconf hive.server2.enable.doAs=true
+./hiveserver2 --hiveconf hive.server2.enable.doAs=false
