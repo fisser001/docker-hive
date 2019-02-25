d17fc31b7e933cb97cf32c51116fa7801f6a658a
Fisser <mfisser@sva.de>
2019-02-25 09:22:31 +0100

    change version hive to 2.3.0

--- startup.sh
+++ startup.sh
@@ -9,7 +9,7 @@ hadoop fs -chmod g+w   /user/hive/warehouse
 hdfs dfs -mkdir -p /apps/tez/
 hdfs dfs -chmod -R 777 /apps/tez/
 
-hdfs dfs -put /tmp/apache-tez-0.8.5-bin.tar.gz /apps/tez/
+hdfs dfs -put /tmp/apache-tez-$TEZ_VERSION-bin.tar.gz /apps/tez/
 hdfs dfs -chown -R $HDFS_USER:$HADOOP_USER /apps
 hadoop fs -chmod g+w /apps/tez
 hadoop fs -chmod g+w /apps/tez/*
