6864e66675158638f34eea9d3069b1f2faaa42b5
Fisser <mfisser@sva.de>
2019-02-26 10:22:31 +0100

    update

--- startup.sh
+++ startup.sh
@@ -15,5 +15,6 @@ hdfs dfs -chown -R $HDFS_USER:$HADOOP_USER /apps
 hadoop fs -chmod g+w /apps/tez
 hadoop fs -chmod g+w /apps/tez/*
 hdfs dfs -chmod -R 777 /apps/tez/
+hdfs dfs -chmod -R 777 /tmp/
 cd $HIVE_HOME/bin
 ./hiveserver2 --hiveconf hive.server2.enable.doAs=false
