6588da0072544e06b51cee59b2d11f7db2213aa9
Fisser <mfisser@sva.de>
2019-02-26 17:06:44 +0100

    test

--- startup.sh
+++ startup.sh
@@ -10,7 +10,7 @@ hdfs dfs -mkdir -p /apps/tez/
 hdfs dfs -chmod -R 777 /apps/tez/
 
 #hdfs dfs -put /apache-tez-$TEZ_VERSION-src/tez-dist/target/tez-$TEZ_VERSION.tar.gz /apps/tez
-hdfs dfs -put /tmp/apache-tez-$TEZ_VERSION-bin.tar.gz /apps/tez/
+hdfs dfs -put /tmp/tez-$TEZ_VERSION-minimal.tar.gz /apps/tez/
 hdfs dfs -chown -R $HDFS_USER:$HADOOP_USER /apps
 hadoop fs -chmod g+w /apps/tez
 hadoop fs -chmod g+w /apps/tez/*
